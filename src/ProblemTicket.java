cf193c781285028765ecea1829ffc4bab26ad462
Lope <Lope922@gmail.com>
2015-03-30 14:15:36 -0500

    got Jlist to accept proper input , but still needs to handle errors. Also need to resolve tickets yet.

--- src/ProblemTicket.java
+++ src/ProblemTicket.java
@@ -1,11 +1,15 @@
 import javax.swing.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
  * Created by Lope on 3/24/2015.
  */
-public class ProblemTicket extends TicketManager {
+public class ProblemTicket {
+
 
     //problem ticket class needs to hold the issue , name of th person recorded it, date resolved , and date issued, as
     // well as severity of the ticket
@@ -20,56 +24,44 @@ public class ProblemTicket extends TicketManager {
     private int severity;
 
     // date it was entered
-    private Date dateEntered;
+    private String dateEntered; // but in a date time format
 
     // date it was resolved;
-    private Date dateResolved;
+    private DateFormat dateResolved;
+
+
+    // Constructor for date time format
+    DateFormat dateFormat = new SimpleDateFormat("MM/dd/yy");
+
+    // looking into a calendar with date time picker i realized that i needed to import a dateformat, calendar
+    // as well as a couple other things.
+    //http://www.mkyong.com/java/java-how-to-get-current-date-time-date-and-calender/
 
 
     // 2 different constructors to either add or remove a ticket
     // to enter a ticket
-    public ProblemTicket(String workerName, String issueDicription, int severity, Date dateEntered) {
-        this.workerName = workerName;
-        this.issueDicription = issueDicription;
-        this.severity = severity;
-        this.dateEntered = dateEntered;
+    public ProblemTicket(String workerName, String issueDicription, int severity, String dateEntered) {
+        this.workerName = workerName; // i would like to set this equal to textbox with username
+        this.issueDicription = issueDicription; // set this equal to text box of issue description.
+        this.severity = severity; // combo dropdown box selected value.
+        this.dateEntered = dateEntered; // set this equal to users entered date.
     }
 
     //to resolve a ticket
-    public ProblemTicket(String workerName, String issueDicription, int severity, Date dateEntered, Date dateResolved) {
+    public ProblemTicket(String workerName, String issueDicription, int severity, String dateEntered, DateFormat dateResolved) {
         this.workerName = workerName;
         this.issueDicription = issueDicription;
         this.severity = severity;
         this.dateEntered = dateEntered;
         this.dateResolved = dateResolved;
     }
-// needs to be able to communicate with the ticket manager in order to get the info in textboxes. ?
 
-    public String getWorkerName() {
 
-        return workerName;
+    @Override
+    public String toString() {
+
+        return (" Reporter name: " + this.workerName + " Severity Of Problem: " + this.severity + " Problem Description: " + this.issueDicription +
+                " Date called in : " + this.dateEntered);
+
     }
-
-    public String getIssueDicription() {
-        return issueDicription;
-    }
-
-    public int getSeverity() {
-        return severity;
-    }
-
-    public Date getDateEntered() {
-        return dateEntered;
-    }
-
-    // store the problem tickets in an array List and also in a JList
-//    public static ProblemTicket AddToBothLists() {
-
-
-        ArrayList<ProblemTicket> problemTickets = new ArrayList<ProblemTicket>();
-
-// get the necessary components to get the build the ticket item to add it to the list
-
-// should recieve a username, description of the problem, severity level , and a date entered to be able to add it to the list
-        // once it meets these requirements add that problem ticket to the list.
-    }
+}
--- src/TicketManager.java
+++ src/TicketManager.java
@@ -1,38 +1,102 @@
+import javax.activity.InvalidActivityException;
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by Lope on 3/24/2015.
  */
 public class TicketManager extends JFrame {
 
+
     private JPanel rootPanel; // JPanel is rootPanel. good practice for now. when i have other pop up windows
     // they will be labeled differently.
 
-    private JTextField userName; // where user inputs the
+    private JTextField userName; // where user inputs their name
 
-    private JTextField descriptionOfProblem;
-    private JComboBox severityLevel; //todo create an enum value to store in severity level
-    private JList list1;
-    private JButton addTicketButton;
-    private JSpinner spinner1;
-    private JSpinner spinner2;
-public enum severityNumbers {one,two,three,four,five}
+    private JTextField descriptionOfProblem; // a breif description of problem
+    private JComboBox severityLevel; // combo dropdown box for severity of problem
+    private JList list1; // list used to store information
+    private JButton addTicketButton; // button to add items to list
+    private JTextField dateTextFieldEntered; // user input box for date problem was recorded.
+
+    private JSpinner spinner1; // not used anymore. was going to use for calandar date picker.
+    //  private JSpinner spinner2;
+
+    // use this list for the gui form
+    DefaultListModel<ProblemTicket> problemTicketDefaultListModel;
+
 
     // setup the ticketmanager window layout and set its visibility to true.
     protected TicketManager() {
         super("Ticket Manager App");
+
         setContentPane(rootPanel);
         pack();
         setVisible(true);
 
-        // will need access to Problem Ticket ?
-    ProblemTicket problemTicket;
+        // this is requesting that the date be types in the mm/dd/yy format.
+        DateFormat userDateInput = new SimpleDateFormat("MM/dd/yy"); //TODO CREATE A METHOD THAT CHECKS FOR PROPER INPUT
+
+        // set the default action when closed to not consume resources.
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // close the applicatin and resources when closing the window
+
+        problemTicketDefaultListModel = new DefaultListModel<ProblemTicket>();
+        // declaring list1 as a model that contains PROBLEM TICKETS
+        list1.setModel(problemTicketDefaultListModel);
 
 
-        // add the action listener that listens for when the button is called.
+        // populate the combo box
+        final int one = 1;
+        final int two = 2;
+        final int three = 3;
+        final int four = 4;
+        final int five = 5;
+
+        severityLevel.addItem(one);
+        severityLevel.addItem(two);
+        severityLevel.addItem(three);
+        severityLevel.addItem(four);
+        severityLevel.addItem(five);
+
+
+//public static String buildInfoFromGUI() { // don't know how to build a method to interpret info. ??? //todo
+
+
+
+
+        // add the action listener that listens for when the button is clicked.
         // this code defines a class, method, and creates an object all in this itty bitty block.
         addTicketButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String Name = userName.getText(); // reporter name is the name entered in text box
+                // get the rest of the arguments for the problem ticket.
+
+                String issueDiscription = descriptionOfProblem.getText(); // description of problem textbox
+
+                int severity = severityLevel.getSelectedIndex()+ 1; // rank of problem from combo box
+
+                // get date time format entered in textbox for date
+                String dateEntered = dateTextFieldEntered.getText(); // date from text box entered
+
+                ProblemTicket userInput = new ProblemTicket (Name, issueDiscription,severity,dateEntered);
+
+              //  list1.add(infoFromForm);
+                problemTicketDefaultListModel.addElement(userInput);
+
+//                try {
+//                    // todo write this code
+//                } catch (NullPointerException iae) {
+//                    JOptionPane.showMessageDialog(TicketManager.this, "we are missing some information here !" + iae);
+//                }
+//
+            }
 
 // add a new ticket to the list and the array list.
 
@@ -42,11 +106,4 @@ public enum severityNumbers {one,two,three,four,five}
 
     }
 }
-
-//todo map the buttons to commands
-
-//todo Create a new main class to run this code.
-
-//todo add a quit button
-
 
